7565a2157608c44e9631df244fe32486b07808bb
ForsakenDe7usion <2430385640cy@gmail.com>
2024-04-18 08:25:08 +0800

    LinkedListDeque add addtional func

--- proj1/deque/Test.java
+++ proj1/deque/Test.java
@@ -3,9 +3,13 @@ package deque;
 public class Test {
 
     public static void main(String[] args) {
-        LinkedListDeque<Integer> list = new LinkedListDeque<Integer>();
+        LinkedListDeque<Integer> list = new LinkedListDeque<>();
+        list.addFirst(1);
+        list.addFirst(3);
+        list.addFirst(5);
         list.addFirst(2);
-        list.addFirst(2);
+        list.printDeque();
+        System.out.print(list.size);
 
     }
 }
