f629d5f4f0562dcc42a2d235b0a52a762ce7e9ff
ForsakenDelusion <wangcy0205@outlook.com>
2024-04-19 00:37:21 +0800

    ArraDequeTest

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -23,6 +23,45 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        int[] NsArray = new int[]{1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        int M = 10000; // M operations
+
+        for (int n : NsArray) {
+            // 1. Create an SLList.
+            SLList<Integer> sl = new SLList<>();
+
+            // 2. Add N items to the SLList.
+            doAddLastNTimes(sl, n);
+
+            // 3. Start the timer.
+            Stopwatch sw = new Stopwatch();
+
+            // 4. Perform M getLast operations on the SLList.
+            doGetLastNTimes(sl, M);
+
+            // 5.Check the timer. This gives the total time to complete all M operations.
+            double timeInSeconds = sw.elapsedTime();
+
+            Ns.addLast(n);
+            times.addLast(timeInSeconds);
+            opCounts.addLast(M);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 
+    private static void doGetLastNTimes(SLList<Integer> sl, int ops) {
+        for (int i = 0; i < ops; i++) {
+            sl.getLast();
+        }
+    }
+
+    private static void doAddLastNTimes(SLList<Integer> sl, int n) {
+        for (int i = 0; i < n; i++) {
+            sl.addLast(0);
+        }
+    }
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -1,5 +1,89 @@
 package deque;
 
+import org.checkerframework.checker.units.qual.A;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class ArrayDequeTest {
-    
+    @Test
+    public void addIsEmptySizeTest(){
+        ArrayDeque<String> arrayTest = new ArrayDeque<>();
+
+        assertTrue("刚初始化的队列应该是空的",arrayTest.isEmpty());
+        arrayTest.addFirst("前面");
+
+        assertEquals(1,arrayTest.size());
+        assertFalse("arrayTest应该包含一个元素",arrayTest.isEmpty());
+
+        arrayTest.addLast("中间");
+        assertEquals(2,arrayTest.size());
+
+        arrayTest.addLast("后面");
+        assertEquals(3,arrayTest.size());
+
+        System.out.println("Deque:");
+        arrayTest.printDeque();
+    }
+
+    @Test
+    public void addRemoveTest(){
+        ArrayDeque<Integer> arrayTest = new ArrayDeque<>();
+        assertTrue("刚初始化的队列应该是空的",arrayTest.isEmpty());
+
+        arrayTest.addFirst(10);
+        assertFalse("arrayTest应该包含一个元素",arrayTest.isEmpty());
+
+        arrayTest.removeFirst();
+        assertTrue("移除操作结束之后队列应该是空的",arrayTest.isEmpty());
+    }
+    @Test
+    public void removeEmptyTest(){
+
+        ArrayDeque<Integer> arrayTest = new ArrayDeque<>();
+        arrayTest.addFirst(3);
+
+        arrayTest.removeLast();
+        arrayTest.removeFirst();
+        arrayTest.removeLast();
+        arrayTest.removeFirst();
+
+        String errorMsg = "size不对";
+        errorMsg += "size()返回的是"+arrayTest.size+"\n";
+        errorMsg += "实际的size()应该返回0";
+
+        assertEquals(errorMsg,0,arrayTest.size);
+
+    }
+
+    @Test
+    public void multipleParamTest(){
+        ArrayDeque<String> array1 = new ArrayDeque<>();
+        ArrayDeque<Double> array2 = new ArrayDeque<>();
+        ArrayDeque<Boolean> array3 = new ArrayDeque<>();
+
+        array1.addFirst("string");
+        array2.addFirst(3.14159);
+        array3.addFirst(true);
+
+        String s = array1.removeFirst();
+        double d = array2.removeFirst();
+        boolean b = array3.removeFirst();
+    }
+
+    public void bigArrayDequeTest(){
+
+        ArrayDeque<Integer> arrayTest = new ArrayDeque<>();
+        for (int i = 0; i < 1000000; i++) {
+            arrayTest.addLast(i);
+        }
+
+        for (double i = 0; i < 500000; i++) {
+            assertEquals("应该具有相同的值", i, (double) arrayTest.removeFirst(), 0.0);
+        }
+
+        for (double i = 999999; i > 500000; i--) {
+            assertEquals("应该具有相同的值", i, (double) arrayTest.removeLast(), 0.0);
+        }
+    }
+
 }
