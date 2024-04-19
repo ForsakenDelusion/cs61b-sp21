d0ad856dd625422befde3a985e8765fc305e6121
ForsakenDelusion <wangcy0205@outlook.com>
2024-04-19 16:36:25 +0800

    add override tag

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -28,7 +28,7 @@ public class LinkedListDeque<T> implements Deque<T>,Iterable<T> {
         sentinel.pri = sentinel;
         size = 0;
     }
-
+    @Override
     public void addFirst(T item){
         if(this.isEmpty()){
             sentinel.next = new Node<>(item,sentinel,sentinel.pri);
@@ -42,7 +42,7 @@ public class LinkedListDeque<T> implements Deque<T>,Iterable<T> {
         }
         size+=1;
     }
-
+    @Override
     public void addLast(T item){
         if(this.isEmpty()){
             sentinel.next = new Node<>(item,sentinel,sentinel.pri);
@@ -56,15 +56,15 @@ public class LinkedListDeque<T> implements Deque<T>,Iterable<T> {
         }
         size+=1;
     }
-
+    @Override
     public boolean isEmpty(){
         return size()==0;
     }
-
+    @Override
     public int size(){
         return size;
     }
-
+    @Override
     public void printDeque(){
         int count = size;
         Node<T> curNode = sentinel.next;
@@ -74,7 +74,7 @@ public class LinkedListDeque<T> implements Deque<T>,Iterable<T> {
         }
         System.out.println();
     }
-
+    @Override
     public T removeFirst(){
         if(this.isEmpty()){
             return null;
@@ -88,7 +88,7 @@ public class LinkedListDeque<T> implements Deque<T>,Iterable<T> {
             return removeElem;
         }
     }
-
+    @Override
     public T removeLast(){
         if(this.isEmpty()){
             return null;
@@ -103,7 +103,7 @@ public class LinkedListDeque<T> implements Deque<T>,Iterable<T> {
             return removeElem;
         }
     }
-
+    @Override
     public T get(int index){
         Node<T> curNode = sentinel;
         for(;index>0;index--){
@@ -137,6 +137,32 @@ public class LinkedListDeque<T> implements Deque<T>,Iterable<T> {
             count-=1;
             return this.get(count);
         }
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null) {
+            return false;
+        }
+
+        if (!(o instanceof Deque)) {
+            return false;
+        }
+
+        Deque<T> other = (Deque<T>) o;
+        if (this.size() != other.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < size(); i++) {
+            if (!this.get(i).equals(other.get(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
