3840f23085c0b0c16f760b4dee4234d8d8bd8ead
ForsakenDelusion <2430385640cy@gmail.com>
2024-04-22 12:23:41 +0800

    fix style

--- proj1/gh2/GuitarHero.java
+++ proj1/gh2/GuitarHero.java
@@ -5,42 +5,44 @@ import edu.princeton.cs.algs4.StdDraw;
 public class GuitarHero {
 
 
-    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
-    public static final double CONCERT_A = 440.0;
+    private static final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    public static final double CONCERT = 440.0;
+    public static final double PARAM24 = 24.0;
+    public static final double PARAM12 = 12.0;
 
 
-    public static double CONCERTKEY(int index){
-        return 440 * Math.pow(2, (index - 24) / 12);
+    public static double concertKey(int index) {
+        return CONCERT * Math.pow(2, (index - PARAM24) / PARAM12);
     }
 
     public static void main(String[] args) {
 
-        GuitarString[] strings = new GuitarString[keyboard.length()];
+        GuitarString[] strings = new GuitarString[KEYBOARD.length()];
 
-        for (int i = 0; i < keyboard.length(); i++) {
-            double newCONCERT = CONCERTKEY(i);
+        for (int i = 0; i < KEYBOARD.length(); i++) {
+            double newCONCERT = concertKey(i);
             strings[i] = new GuitarString(newCONCERT);
         }
 
-        while(true){
-            if(StdDraw.hasNextKeyTyped()){
+        while (true) {
+            if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                int index = keyboard.indexOf(key);
-                if(index>=0){
+                int index = KEYBOARD.indexOf(key);
+                if (index >= 0) {
                     strings[index].pluck();
                 }
             }
 
             double sample = 0.0;
 
-            for(GuitarString index:strings){
-                sample=sample+index.sample();
+            for (GuitarString index:strings) {
+                sample = sample + index.sample();
             }
 
 
             StdAudio.play(sample);
 
-            for(GuitarString index:strings){
+            for (GuitarString index:strings) {
                 index.tic();
             }
 
--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,15 +1,15 @@
 package gh2;
 
-// TODO: uncomment the following import once you're ready to start this portion
-// import deque.Deque;
-// TODO: maybe more imports
-
+// uncomment the following import once you're ready to start this portion
 import deque.ArrayDeque;
 import deque.Deque;
+// maybe more imports
+
+
 
 
 //Note: This file will not compile until you complete the Deque implementations
-public class GuitarString{
+public class GuitarString {
     /** Constants. Do not change. In case you're curious, the keyword final
      * means the values cannot be changed at runtime. We'll discuss this and
      * other topics in lecture on Friday. */
@@ -17,17 +17,17 @@ public class GuitarString{
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
+    // uncomment the following line once you're ready to start this portion
     private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
+        //       Create a buffer with capacity = SR / frequency. You'll need to
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
 
-        int capacity = (int)Math.round(SR / frequency);
+        int capacity = (int) Math.round(SR / frequency);
         buffer = new ArrayDeque<>();
         for (int i = 0; i < capacity; i++) {
             buffer.addFirst(0.0);
@@ -39,7 +39,7 @@ public class GuitarString{
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
+        //       Dequeue everything in buffer, and replace with random numbers
         //       between -0.5 and 0.5. You can get such a number by using:
         //       double r = Math.random() - 0.5;
         //
@@ -59,16 +59,16 @@ public class GuitarString{
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
+        //      Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       **Do not call StdAudio.play().**
-        buffer.addLast(DECAY*(buffer.removeFirst()+sample())/2);
+        buffer.addLast(DECAY * (buffer.removeFirst() + sample()) / 2);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
+        // Return the correct thing.
         return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
+    // Remove all comments that say TODO when you're done.
