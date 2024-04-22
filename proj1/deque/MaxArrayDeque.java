ee674cb892592c37c5368df2c4773263ce04db91
ForsakenDelusion <2430385640cy@gmail.com>
2024-04-22 11:37:25 +0800

    fix style

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,25 +1,25 @@
 package deque;
 import java.util.Comparator;
-public class MaxArrayDeque<T> extends ArrayDeque<T>{
-    private Comparator<T> cmp;//定义MaxArrayDeque的Comparator为cmp
-    public MaxArrayDeque (Comparator<T> c){
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private Comparator<T> cmp; //定义MaxArrayDeque的Comparator为cmp
+    public MaxArrayDeque(Comparator<T> c) {
         super();
-        this.cmp = c;//这样就是让MaxArrayDeque的Comparator等于外面传进来的c
+        this.cmp = c; //这样就是让MaxArrayDeque的Comparator等于外面传进来的c
     }
 
 
-    public T max(){
+    public T max() {
         return max(cmp);
     }
 
-    public T max(Comparator<T> c){
-        if(this.isEmpty()){
+    public T max(Comparator<T> c) {
+        if (this.isEmpty()) {
             return null;
         }
 
         int index = 0;
         for (int count = 0; count < this.size(); count++) {
-            if(cmp.compare(get(index),get(count))<0){
+            if (cmp.compare(get(index), get(count)) < 0) {
                 index = count;
             }
 
