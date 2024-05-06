6d7d3c4c8ad456e62cad1bbcb251b812b5e9e889
ForsakenDelusion <wangcy0205@outlook.com>
2024-05-06 17:32:25 +0800

    all done

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,40 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> AL = new AListNoResizing<>();
+        BuggyAList<Integer> BL = new BuggyAList<>();
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform (0, 100);
+                AL.addLast(randVal);
+                BL.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+                assertEquals(AL.getLast(), BL.getLast());
+            } else if (operationNumber == 1) {
+                // size
+                int ALsize = AL.size();
+                int BLsize = BL.size();
+                System.out.println("ALsize: " + ALsize + " BLsize: " + BLsize);
+                assertEquals(ALsize, BLsize);
+            } else if (AL.size() > 0 && BL.size() > 0) {
+                if (operationNumber == 2) {
+                    int ALlast = AL.getLast();
+                    int BLlast = BL.getLast();
+                    System.out.println("the AL last number is " + ALlast + ", the BL last number is " + BLlast);
+                    assertEquals(ALlast, BLlast);
+                } else if (operationNumber == 3) {
+                    int removeALlast = AL.removeLast();
+                    int removeBLlast = BL.removeLast();
+                    assertEquals(removeALlast, removeBLlast);
+                    assertEquals(AL.size(), BL.size());
+                }
+            }
+
+        }
+    }
 }
