335eee07693d70c0e3ed81690e044db20505aeed
ForsakenDe7usion <2430385640cy@gmail.com>
2024-06-04 21:50:54 +0800

    basic idea for add

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -1,6 +1,39 @@
 package gitlet;
 
-public class Blob {
+import java.io.File;
+import java.io.Serializable;
+
+import static gitlet.Repository.CWD;
+import static gitlet.Repository.GITLET_OBJECTS;
+import static gitlet.Utils.*;
+
+public class Blob implements Serializable {
     String fileName;
     String content;
+
+    public String getContent(){
+        return this.content;
+    }
+
+    public Blob(String fileName, String content){
+        this.fileName = fileName;
+        this.content = content;
+    }
+
+    static Blob createBlob(String fileName){
+        File CWDFile = new File(CWD,fileName);
+        String content = readContentsAsString(CWDFile);
+        Blob newBlob =  new Blob(fileName,content);
+        newBlob.saveBlob();
+        return newBlob;
+
+    }
+
+    static String getBlobSHA1(String content){
+        return sha1(content);
+    }
+
+    public void saveBlob(){
+        writeObject(join(GITLET_OBJECTS,getBlobSHA1(this.getContent())),this);
+    }
 }
--- proj2/gitlet/Index.java
+++ proj2/gitlet/Index.java
@@ -1,12 +1,29 @@
 package gitlet;
 
-public class Index {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import static gitlet.Repository.GITLET_INDEX;
+import static gitlet.Utils.*;
 
 
+public class Index implements Serializable {
 
+    ArrayList<Blob> blobArry = new ArrayList<>();
 
+    Index(){
+        this.saveIndex();
+    }
 
-    public Index(){
+    public ArrayList<Blob> getBlobArry() {
+        return this.blobArry;
+    }
 
+    static void add(Blob blob){
+        readObject(GITLET_INDEX,Index.class).getBlobArry().add(blob);
+    }
+
+    void saveIndex(){
+        writeObject(GITLET_INDEX,Index.class);
     }
 }
