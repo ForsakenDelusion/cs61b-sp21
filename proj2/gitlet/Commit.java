5728b23f20eb01f4ec920b87769b55cf755a754c
ForsakenDe7usion <2430385640cy@gmail.com>
2024-06-04 20:40:19 +0800

    init

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -7,7 +7,9 @@ import java.io.Serializable;
 import java.util.Date; // TODO: You'll likely use this in this class
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
+import static gitlet.Repository.GITLET_DIR;
 import static gitlet.Utils.*;
 
 /** Represents a gitlet commit object.
@@ -26,11 +28,45 @@ public class Commit implements Serializable {
      */
 
     /** The message of this Commit. */
+    String id;
     String message;
     String parentCommit;
     String branch;
+    String date;
     Map<String, Blob> Blobs = new HashMap<>();
     /* TODO: fill in the rest of this class. */
+
+    Commit(String message){
+        this.message = message;
+        this.date = new Date().toString();
+        this.branch = getCurrentCommit().getBranch();
+        this.parentCommit = getCurrentCommit().getId();
+    }
+
+    Commit(){
+        this.date = "00:00:00 UTC, Thursday, 1 January 1970";
+        this.parentCommit = null;
+    }
+
+    /** Get current commit */
+    static Commit getCurrentCommit() {
+        return readObject(join(GITLET_DIR,"HEAD"),Commit.class);
+    }
+
+    /** parentId getter */
+    String getParentCommit(){
+        return this.parentCommit;
+    }
+
+    /** Id getter */
+    String getId(){
+        return this.id;
+    }
+
+    /** Branch getter */
+    String getBranch(){
+        return this.branch;
+    }
 
 
 
