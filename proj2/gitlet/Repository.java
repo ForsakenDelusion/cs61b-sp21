a920507ab160cd3cdd2f9b6f74d547b6e1d4ae55
Forsaken Delusion <2430385640cy@gmail.com>
2024-06-18 03:12:14 +0800

    fix reset bug

--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -428,7 +428,6 @@ public class Repository {
                 if (!areBlobsEqual(givenCommitBlob, splitCommitBlob) && !areBlobsEqual(curCommitBlob, splitCommitBlob) && !areBlobsEqual(givenCommitBlob, curCommitBlob)) {
                     String newContent = getString(curCommitBlob, givenCommitBlob);
                     writeContents(splitCommitFile, newContent);
-                    add(splitCommitFile.getName());
                     flag = true;
                     curCommitMap.remove(splitCommitFile);
                     givenCommitMap.remove(splitCommitFile);
